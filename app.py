15da2b94418268739ede4fee062a9da8d4d2bbb2
Moș Daniele <dany.mosh@yahoo.com>
2023-07-17 00:33:10 +0300

    added conversational memory

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 # declare an environment variable mapped to the according key
 os.environ['OPENAI_API_KEY'] = api_key_openai
@@ -24,14 +25,17 @@ research_template = PromptTemplate(
 
 topic_template = PromptTemplate(
     input_variables= ['research'],
-    template = 'Choose from these research topics {research} what is the newest and most relevent at the moment. Expand upon it'
+    template = 'Choose from the following research topics what is the newest and most relevent at the moment and expand upon it: {research}'
 )
+
+# Memory
+memory = ConversationBufferMemory(input_key='topic', memory_key='chat_memory')
 
 # LLMs
 creativity_indicator = 0.8 # indicates how creative the LLMs responses are 
 llm = OpenAI(temperature = creativity_indicator)
-research_chain = LLMChain(llm=llm, prompt=research_template, verbose=True, output_key='research')
-title_chain = LLMChain(llm=llm, prompt=topic_template, verbose=True, output_key='topics')
+research_chain = LLMChain(llm=llm, prompt=research_template, verbose=True, output_key='research', memory=memory)
+title_chain = LLMChain(llm=llm, prompt=topic_template, verbose=True, output_key='topics', memory=memory)
 seq_chain = SequentialChain(chains=[research_chain, title_chain], verbose=True,
                             input_variables=['topic'], output_variables=['research', 'topics'])
 
@@ -44,3 +48,6 @@ if prompt:
     # response_handler.post_process_list_response()
     st.write(response['research'])
     st.write(response['topics'])
+
+    with st.expander('History'):
+        st.info(memory.buffer)
