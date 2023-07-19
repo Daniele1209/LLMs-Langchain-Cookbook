a2af23c2c7321e79d0def515d710d2d7845ba47c
Moș Daniele <dany.mosh@yahoo.com>
2023-07-19 21:20:34 +0300

    'very' keyword made the post way to short

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # STRING DEFINITION
 first_request = 'Write a short list about scientific fun facts on the topic of {topic}'
-second_request = 'Choose one of the topic from the list: {research}. Write it very short for instagram post without hashtags. Validate it using thw following information {wikipedia_research}'
+second_request = 'Choose one of the topic from the list: {research}. Write it short for instagram post without hashtags. Validate it using thw following information {wikipedia_research}'
 third_request = 'Using the description of the {post} give me suitable hashtags.'
 
 # TITLE AND DESCRIPTION
