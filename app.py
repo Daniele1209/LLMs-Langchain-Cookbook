155df0632c4207225f634bbfafab182d0d6e1acf
Moș Daniele <dany.mosh@yahoo.com>
2023-07-19 19:46:59 +0300

    general app reconfiguration + new env configuration for keys

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from api_key import api_key_openai
 from response_handler import ResponseHandler
+from config import first_request, second_request, title_string, description_string
 
 import streamlit as st
 from langchain.llms import OpenAI
@@ -8,36 +8,45 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
+from dotenv import load_dotenv
 
-# declare an environment variable mapped to the according key
-os.environ['OPENAI_API_KEY'] = api_key_openai
+load_dotenv()
 
 # --- Streamlit website part below ---
 
-st.title('LLM Showcase ☄️') 
+st.title(title_string) 
 # input field for prompts
-prompt = st.text_input('State-of-the-art on the topic of:')
+# prompt = st.text_input('State-of-the-art on the topic of:')
+prompt = st.text_input(description_string)
 
 # Prompt templates
 research_template = PromptTemplate(
     input_variables= ['topic'],
-    template = 'What is the state-of-the-art on the topic of {topic}. Write it as a list'
+    # template = 'What is the state-of-the-art on the topic of {topic}. Write it as a list'
+    template = first_request
 )
 
 topic_template = PromptTemplate(
     input_variables= ['research', 'wikipedia_research'],
-    template = 'Choose from the following research topics what is the newest and most relevent at the moment, and expand upon it: {research}. Validate it using the following information: {wikipedia_research}'
+    template = second_request
+)
+
+hashtags_template = PromptTemplate(
+    input_variables= ['post'],
+    template = second_request
 )
 
 # Memory
-research_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_memory')
+# research_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_memory')
 topics_memory = ConversationBufferMemory(input_key='research', memory_key='chat_memory')
+hashtag_memory = ConversationBufferMemory(input_key='topics', memory_key='chat_memory')
 
 # LLMs
 creativity_indicator = 0.8 # indicates how creative the LLMs responses are 
 llm = OpenAI(temperature = creativity_indicator)
-research_chain = LLMChain(llm=llm, prompt=research_template, verbose=True, output_key='research', memory=research_memory)
+research_chain = LLMChain(llm=llm, prompt=research_template, verbose=True, output_key='research')
 topics_chain = LLMChain(llm=llm, prompt=topic_template, verbose=True, output_key='topics', memory=topics_memory)
+hashtags_chain = LLMChain(llm=llm, prompt=hashtags_template, verbose=True, output_key='hashtags', memory=hashtag_memory)
 
 # seq_chain = SequentialChain(chains=[research_chain, topics_chain], verbose=True,
 #                             input_variables=['topic'], output_variables=['research', 'topics'])
@@ -52,16 +61,20 @@ if prompt:
     # response_handler.post_process_list_response()
     research = research_chain.run(prompt)
     wiki_research = wiki.run(prompt)
-    topics = topics_chain.run(research=research, wikipedia_research=wiki_research)
+    post = topics_chain.run(research=research, wikipedia_research=wiki_research)
+    hashtags = hashtags_chain.run(post=post)
 
     st.write(research)
-    st.write(topics)
+    st.write(post)
 
-    with st.expander('Research History'):
-        st.info(research_memory.buffer)
+    # with st.expander('Research History'):
+    #     st.info(research_memory.buffer)
 
     with st.expander('Topic History'):
         st.info(topics_memory.buffer)
+
+    with st.expander('Hashtags History'):
+        st.info(hashtag_memory.buffer)
 
     with st.expander('Wiki History'):
         st.info(wiki_research)
---
+++ config.py
@@ -0,0 +1,8 @@
+# STRING DEFINITION
+first_request = 'Write a short list about scientific fun facts on the topic of {topic}'
+second_request = 'Choose one of the topic from the list: {research}. Write it in short as an instagram post without hashtags. Validate it using thw following information {wikipedia_research}'
+third_request = 'Using the description of the {post} give me suitable hashtags.'
+
+# TITLE AND DESCRIPTION
+title_string = 'Instagram-inatorul ☄️'
+description_string = 'Give your topic as an input here to get the Instagram scientific post:'
